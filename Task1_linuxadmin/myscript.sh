fc47391f7788237b7d22b48d7d1363127304aea9
Ammarkhalil07 <Ammarkhalil707@gmail.com>
2024-02-25 09:37:46 +0200

    first commit

---
+++ Task1_linuxadmin/myscript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir /home/myDirectory
+mkdir /home/myDirectory/secondDirectory
+touch /home/myDirectory/secondDirectory/myNotePaper
+cp /home/myDirectory/secondDirectory/myNotePaper /home/myDirectory
+mv /home/myDirectory/myNotePaper /home/myDirectory/myOldNotePaper
