5652099dee0d9b3f939b0f8b74ebe1e788c43b1f
Ammar Khalil <94346150+Ammarkhalil07@users.noreply.github.com>
2024-03-01 09:53:20 +0200

    Delete Task1_linuxadmin directory

--- Task1_linuxadmin/myscript.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir /home/myDirectory
-mkdir /home/myDirectory/secondDirectory
-touch /home/myDirectory/secondDirectory/myNotePaper
-cp /home/myDirectory/secondDirectory/myNotePaper /home/myDirectory
-mv /home/myDirectory/myNotePaper /home/myDirectory/myOldNotePaper
